6638737cd163d1edc214a6a2f0fcca3d52af366b
kyka123 <kyka123@op.pl>
2020-05-20 21:08:46 +0200

    first commit

---
+++ 01/app.js
@@ -0,0 +1,77 @@
+// const name = document.getElementById("name");
+// const email = document.getElementById("email");
+// const question = document.getElementById("question");
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const [name, email, question] = e.srcElement;
+  if (validate(name.value, email.value, question.value)) {
+    send(name.value, email.value, question.value);
+    clearInputs([name, email, question]);
+  }
+});
+
+function clearInputs(inputs) {
+  inputs.forEach((element) => (element.value = ""));
+}
+
+function validate(name, email, question) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (!name) return false;
+  if (!re.test(email)) return false;
+  if (!question) return false;
+  return true;
+}
+
+function sendEmail(params) {
+  const service = "gmail";
+  const templateId = "zapytanie_z_formularza";
+  const user_id = "user_7UmOln4rmGnWztCUbINrT";
+  window.emailjs
+    .send(service, templateId, params, user_id)
+    .then(() => console.log("Email Wysłany pomyślnie"))
+    .catch((err) => throwError(err))
+    .finally(() => setLoading(false));
+}
+
+function send(name, email, question) {
+  setLoading(true);
+  const clientParams = {
+    to_email: email,
+    from_name: "Krzysztof Kasprzyk",
+    subject: `Formularz Zadaj pytanie`,
+    content: `
+      <h1>Cześć, ${name}</h1>
+      <p>Dzięki, że zadałeś bardzo ciekawe pytanie w formularzu</p>
+      <p>pytanie: ${question}</p>
+      <p>Serdercznie Pozdrawam, Krzysztof Kasprzyk</p>
+    `,
+  };
+
+  const myParams = {
+    to_email: `krzysztofkasprzyk.kyka123@gmail.com`,
+    from_name: name,
+    subject: `Ktoś zadał pytanie w tym twoim formularzu`,
+    content: `
+    <h1>Szybko odpisz</h1>
+    <p>imię: ${name}</p>
+    <p>email: ${email}</p>
+    <p>pytanie: ${question}</p>
+    <p>Chyba on pozdrawia</p>
+    `,
+  };
+  sendEmail(clientParams);
+  sendEmail(myParams);
+}
+
+function throwError(type) {
+  console.log(type);
+  alert("Błąd servera");
+}
+
+function setLoading(loading) {
+  document.getElementById("submit").innerHTML = loading
+    ? "Loading..."
+    : "Wyślij";
+}
---
+++ 03/app.js
@@ -0,0 +1,35 @@
+const template_params = {
+  email: "kyka123@op.pl",
+  name: "Jakieś imię",
+  message: "question_value",
+};
+
+const sendEmail = () => {
+  emailjs
+    .send(
+      "gmail",
+      "zapytanie_z_formularza",
+      template_params,
+      "user_7UmOln4rmGnWztCUbINrT"
+    )
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+};
+
+function sendEmailUsingApiCall() {
+  const data = {
+    service_id: "gmail",
+    template_id: "zapytanie_z_formularza",
+    user_id: "user_7UmOln4rmGnWztCUbINrT",
+    template_params: template_params,
+  };
+
+  axios
+    .post("https://api.emailjs.com/api/v1.0/email/send", data)
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
