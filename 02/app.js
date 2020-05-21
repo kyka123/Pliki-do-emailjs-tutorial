16efa6bb73bf9d0b3480b75ef72bfbc87ebcd150
kyka123 <kyka123@op.pl>
2020-05-21 15:46:37 +0200

    update title style

--- 02/app.js
+++ 02/app.js
@@ -1,34 +1,29 @@
-const form = document.getElementById("form");
+const form = document.getElementById('form')
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  // sendForm(e);
-  // sendFormUsingApiCall(e);
-});
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    // sendForm(e);
+    // sendFormUsingApiCall(e);
+})
 
 function sendForm(e) {
-  window.emailjs
-    .sendForm(
-      "gmail",
-      "zapytanie_z_formularza",
-      e.target /*albo po prostu "#form"*/,
-      "user_7UmOln4rmGnWztCUbINrT"
-    )
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    window.emailjs
+        .sendForm('gmail', 'zapytanie_z_formularza', e.target /*albo po prostu "#form"*/, 'user_7UmOln4rmGnWztCUbINrT')
+        .then(res => {
+            console.log(res)
+        })
+        .catch(err => {
+            console.log(err)
+        })
 }
 
 function sendFormUsingApiCall(e) {
-  const formData = new FormData(e.target);
-  formData.append("service_id", "gmail");
-  formData.append("template_id", "zapytanie_z_formularza");
-  formData.append("user_id", "user_7UmOln4rmGnWztCUbINrT");
-  axios
-    .post("https://api.emailjs.com/api/v1.0/email/send-form", formData)
-    .then((res) => console.log(res))
-    .catch((err) => console.log(err));
+    const formData = new FormData(e.target)
+    formData.append('service_id', 'gmail')
+    formData.append('template_id', 'zapytanie_z_formularza')
+    formData.append('user_id', 'user_7UmOln4rmGnWztCUbINrT')
+    axios
+        .post('https://api.emailjs.com/api/v1.0/email/send-form', formData)
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
 }
